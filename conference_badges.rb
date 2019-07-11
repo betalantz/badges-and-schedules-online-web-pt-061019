e02f3895f216ebc6670b9c24466fbc4d7be13ead
Lantz Warrick <betalantz@gmail.com>
2019-07-11 17:10:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,11 +20,11 @@ def assign_rooms(attendees)
   assigned_rooms
 end
 
-def assign_rooms(attendees)
-  attendees.collect.with_index(1) do |attendee, index|
-    assigned_rooms << "Hello, #{attendee}! You'll be assigned to room #{index}!"
-  end
-end
+# def assign_rooms(attendees)
+#   attendees.collect.with_index(1) do |attendee, index|
+#     assigned_rooms << "Hello, #{attendee}! You'll be assigned to room #{index}!"
+#   end
+# end
 
 # def assign_rooms(attendees)
 #   counter = 1
