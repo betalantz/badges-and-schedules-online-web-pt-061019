a98c56353b852bdf7d8d2939bdc0501491f237c7
Lantz Warrick <betalantz@gmail.com>
2019-07-11 17:07:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+<<<<<<< HEAD
 def batch_badge_creator(attendees)
   attendees.collect {|attendee| badge_maker(attendee)}
 end
@@ -58,3 +59,36 @@ end
 printer(attendees)
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+=======
+# def batch_badge_creator(attendees)
+#   attendees.map {|person| badge_maker(person)}
+# end
+def batch_badge_creator(attendees)
+  i = []
+  attendees.each do |name|
+    i.push(badge_maker(name))
+  end
+  return i
+end
+
+# def assign_rooms(attendees)
+#   r = []
+#   attendees.each_with_index do |name, room_assignment|
+#     r << "Hello, #{name}! You'll be assigned to room #{room_assignment + 1}!"
+#   end
+#   r
+# end
+
+def assign_rooms(attendees)
+  attendees.collect.with_index(1) { |person, index| "Hello, #{person}! You'll be assigned to room #{index}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
+end
+>>>>>>> 2c6ee22ca7434cd89c150a549420583261d9210b
